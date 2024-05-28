b8e3408ba820f0b763e40b177010c1cb4b462361
Edgar Lopez <edgarlv_1998@hotmail.com>
2024-05-27 23:11:16 -0600

    Transitions in letter and envelope

---
+++ script.js
@@ -0,0 +1,7 @@
+function showLetter () {
+    const envelope = document.querySelector('.envelope-wrapper');
+    envelope.classList.toggle('flap');
+
+    const heart = document.getElementById('heart');
+    heart.classList.toggle('heartMoved');
+}
